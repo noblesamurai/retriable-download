4f89bacdba4c2806a2585c7a1f12a2ecdf31eea6
Tim Allen <tim@noblesamurai.com>
2018-02-09 13:10:16 +1100

    initial version.

--- test/index.js
+++ test/index.js
@@ -1,8 +1,34 @@
 const expect = require('chai').expect;
+const nock = require('nock');
+const download = require('..');
 
 describe('my thing', function () {
-  it('should work', function () {
-    expect(true).to.be.true;
-    throw new Error('unimplemented');
+  let rejected = false;
+  it('rejects immediately on 404', function () {
+    const retries = 3;
+    const scope = nock('http://notthere').get('/thing').reply(404, 'notfound');
+
+    return download(retries, 'http://notthere/thing').catch(() => {
+      rejected = true;
+    }).then(() => {
+      expect(scope.isDone()).to.equal(true);
+      expect(rejected).to.equal(true);
+    });
+  });
+
+  it('retries on transient errors', function () {
+    this.timeout(5000);
+    const retries = 3;
+    const error = new Error('connection reset punk');
+    error.cause = { code: 'ECONNRESET' };
+
+    const scope = nock('http://transient').get('/thing')
+      .twice().replyWithError(error)
+      .get('/thing')
+      .once().reply(200, 'ok');
+
+    return download(retries, 'http://transient/thing').then(() => {
+      expect(scope.isDone(), `still waiting on mocks: ${scope.pendingMocks()}`).to.equal(true);
+    });
   });
 });
